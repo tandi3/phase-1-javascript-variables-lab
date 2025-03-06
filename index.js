9cb6298d0dcdd684eacdfd86b3ec5cb5c56f74a9
tandie oyucho <ysoyucho@gmail.com>
2025-03-06 12:13:29 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+
+
+const companyName = "Scuber"; // Must be a constant
+
+let mostProfitableNeighborhood = "Chelsea"; // Must be defined using let
+
+let companyCeo = "Susan Smith"; // Must be defined using let
